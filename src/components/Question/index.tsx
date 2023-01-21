4e0ab658edd58a7dab894b689cdcab5f2ecbaac6
caseBread <kgu0515@gmail.com>
2023-01-21 20:37:50 +0900

    feat : question title component

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -3,8 +3,9 @@ import BoxContainer from '../common/BoxContainer';
 const Question = () => {
   return (
     <BoxContainer>
-      <div>
-        <input type="text" defaultValue="제목없는 질문" />
+      <div className="flex w-full gap-8">
+        <input className="w-2/3 focus-input p-4 bg-gray-50 border-b-1" type="text" defaultValue="제목없는 질문" />
+        <div className="w-1/3">드롭박스</div>
       </div>
       <div>middle</div>
       <div>bottom</div>
