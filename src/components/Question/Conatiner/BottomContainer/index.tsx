15f463c05229a9dac792d295807bf73fcaf98417
caseBread <kgu0515@gmail.com>
2023-01-22 01:15:36 +0900

    feat : question bottom container 구현
    아직 필수 toggle은 미구현

--- src/components/Question/Conatiner/BottomContainer/index.tsx
+++ src/components/Question/Conatiner/BottomContainer/index.tsx
@@ -1,4 +1,29 @@
+import { MdOutlineContentCopy } from 'react-icons/md';
+import { IoTrashOutline } from 'react-icons/io5';
+import { BiDotsVerticalRounded } from 'react-icons/bi';
+
 const QuestionBottom = () => {
-  return <div>questionBottom</div>;
+  return (
+    <div className="pt-4 w-full border-t-1">
+      <div className="flex justify-end [&>*]:px-2">
+        <button className="hover:bg-gray-100 rounded-full">
+          <MdOutlineContentCopy size={20} />
+        </button>
+        <button className="hover:bg-gray-100 rounded-full">
+          <IoTrashOutline size={20} />
+        </button>
+        <div>
+          <div className="border-l-2 h-full"></div>
+        </div>
+        <div>
+          <span className="mr-2">필수</span>
+          <input id="essential" type="checkbox" />
+        </div>
+        <button className="hover:bg-gray-100 rounded-full">
+          <BiDotsVerticalRounded size={20} />
+        </button>
+      </div>
+    </div>
+  );
 };
 export default QuestionBottom;
--- src/components/common/Dropdown/index.tsx
+++ src/components/common/Dropdown/index.tsx
@@ -5,7 +5,7 @@ interface Props {
 const Dropdown = ({ children }: Props) => {
   return (
     <div>
-      <div className="fixed shadow-md border-1 rounded-md bg-white leading-8 max-w-[14rem] w-full [&>*]:pl-4 [&>*]:py-2 hover:[&>*]:bg-gray-200">
+      <div className="fixed shadow-md border-1 rounded-md bg-white leading-8 max-w-[14rem] w-full [&>*]:pl-4 [&>*]:py-2 hover:[&>*]:bg-gray-200 z-50">
         {children}
       </div>
     </div>
