b3cce8e7edd28ed8aa32060f9d68dada466071db
caseBread <kgu0515@gmail.com>
2023-01-23 00:25:26 +0900

    chore :파일명 변경

--- src/components/Question/Options/ObjectiveOptions/ObjectiveOption/index.tsx
+++ src/components/Question/Options/ObjectiveOptions/ObjectiveOption/index.tsx
@@ -1,28 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FiX } from 'react-icons/fi';
-import { BsCircle, BsSquare } from 'react-icons/bs';
 
-const ObjectiveOption = ({ optionType, id }: { optionType: React.ReactNode; id: number }) => {
-  const [icons, setIcons] = useState<React.ReactNode>();
+const ObjectiveOption = ({ optionType, id }: { optionType: string; id: number }) => {
+  const icon = useIcon({ optionType, id });
 
-  useEffect(() => {
-    switch (optionType) {
-      case '객관식 질문':
-        setIcons(<BsCircle />);
-        break;
-      case '체크박스':
-        setIcons(<BsSquare />);
-        break;
-      case '드롭다운':
-        setIcons(id);
-        break;
-      default:
-        throw new Error('없는 유형입니다.');
-    }
-  }, [id, optionType]);
   return (
     <div className="flex gap-4 items-center">
-      <div className="w-6">{icons}</div>
+      <div className="w-6">{icon}</div>
       <input
         className="focus-input w-full p-2 border-b-1 border-white hover:border-gray-200"
         type="text"
