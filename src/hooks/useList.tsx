00a73456e8a503a1a08d19dac8fa5190bbf1c4e3
caseBread <kgu0515@gmail.com>
2023-01-23 13:12:17 +0900

    refactor : useOptionList -> useList로 변경 및 generic추가

---
+++ src/hooks/useList.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+interface ListT<T> {
+  id: number;
+  value?: T;
+}
+
+const useList = <T,>(): [ListT<T>[], (value: T) => void, (id: number) => void] => {
+  const [list, setList] = useState<ListT<T>[]>([]);
+
+  const addElement = (value: T) => {
+    const newOption: ListT<T> = value === undefined ? { id: list.length + 1 } : { id: list.length + 1, value };
+    setList((prev) => [...prev, newOption]);
+  };
+
+  const removeElement = (id: number) => {
+    setList((prev) => prev.filter((option, idx) => option.id !== id));
+  };
+
+  return [list, addElement, removeElement];
+};
+export default useList;
