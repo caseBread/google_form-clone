8c5e26b1dba9ef57f3737ce425f839cd079f901c
caseBread <kgu0515@gmail.com>
2023-01-29 07:35:59 +0900

    feat : preview에 주관식 input추가

---
+++ src/components/preview/SubjectiveOption/index.tsx
@@ -0,0 +1,8 @@
+const SubjectiveOptionPreview = () => {
+  return (
+    <div>
+      <input className="focus-input border-b-1 w-1/2 p-1" type="text" />
+    </div>
+  );
+};
+export default SubjectiveOptionPreview;
